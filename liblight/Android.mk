39141b1b96ef43b374ef5bf7dc8d68cdd9a7dbfc
Zeeshan <rqmok@hotmail.com>
2013-07-15 16:15:02 +0930

    Updates

--- liblight/Android.mk
+++ liblight/Android.mk
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-ifeq ($(TARGET_PROVIDES_LIBLIGHT),true)
+ifeq ($(TARGET_PROVIDES_LIBLIGHTS),true)
 LOCAL_PATH:= $(call my-dir)
 # HAL module implemenation stored in
 # hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.board.platform>.so
